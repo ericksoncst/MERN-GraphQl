625714ebcad0f386effa3149fae53c29d0d6c315
Erickson Teixeira <rodox.k@hotmail.com>
2019-01-27 20:53:53 -0200

    create Event model and mutation

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/Event');
 
 const app = express();
 
@@ -10,13 +13,28 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -26,17 +44,42 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['SOAD SHOW', 'JINJER SHOW', 'REACT MEETING'];
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc };
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
         },
 
-        createEvent: args => {
-            const eventName = args.name;
-            return eventName;
+    createEvent: args => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date)
+        });
+        return event.save()
+            .then(result => {
+                return { ...result._doc };
+            })
+            .catch(err => { throw err });
+        return event;
         }
+    }, 
 
-    },
     graphiql: true
 }));
 
 const PORT = 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
+mongoose.connect(`
+    ${process.env.MONGO_URI}`,
+    { useNewUrlParser: true })
+    .then(
+        app.listen(PORT, console.log(`Server running on port ${PORT}`))
+    )
+    .catch(err => console.log(err));
