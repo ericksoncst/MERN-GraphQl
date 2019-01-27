9bd749cda9f1284cefc224490e95f153800b5405
Erickson Teixeira <rodox.k@hotmail.com>
2019-01-27 19:06:34 -0200

    first query and mutation

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['SOAD SHOW', 'JINJER SHOW', 'REACT MEETING'];
+        },
+
+        createEvent: args => {
+            const eventName = args.name;
+            return eventName;
+        }
+
+    },
+    graphiql: true
+}));
 
 const PORT = 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
